9922109c0306db471cbc94660e0a10e1b1191309
Aleksey Bykhun <theisease@gmail.com>
2019-04-18 01:38:15 +0300

    ad: "fix" react ad

--- client/src/components/markup/Topbar.js
+++ client/src/components/markup/Topbar.js
@@ -1,7 +1,72 @@
 const { connect } = ReactRedux
 
+class __Topbar extends React.Component {
+  codefundAdRef = null
 
-const __Topbar = (props) => (
+  componentDidMount () {
+
+    // https://codefund.app/properties/260/funder.js
+
+    var evt = new Event('codefund');
+    var uplift = {};
+
+    function trackUplift() {
+      try {
+        var url = 'https://codefund.app/impressions/103e18c0-6fc2-4059-ba16-697223a85d80/uplift?advertiser_id=132';
+        console.log('CodeFund is recording uplift. ' + url);
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.send();
+      } catch (e) {
+        console.log('CodeFund was unable to record uplift! ' + e.message);
+      }
+    };
+
+    function verifyUplift() {
+      if (uplift.pixel1 === undefined || uplift.pixel2 === undefined) { return; }
+      if (uplift.pixel1 && !uplift.pixel2) { trackUplift(); }
+    }
+
+    function detectUplift(count) {
+      var url = 'https://cdn2.codefund.app/assets/px.js';
+      if (url.length === 0) { return; }
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          if (xhr.status >= 200 && xhr.status < 300) {
+            if (count === 1) { detectUplift(2); }
+            uplift['pixel' + count] = true;
+          } else {
+            uplift['pixel' + count] = false;
+          }
+          verifyUplift();
+        }
+      };
+      xhr.open('GET', url + '?ch=' + count + '&rnd=' + Math.random() * 11);
+      xhr.send();
+    }
+
+    debugger;
+    try {
+      var targetElement = this.codefundAdRef;
+
+      targetElement.innerHTML = '<div id="cf"> <span class="cf-wrapper"> <a data-href="campaign_url" class="cf-text" target="_blank" rel="noopener"> <strong>Gitcoin</strong> <span>💰 Use your skills to grow Open Source while getting paid!</span> </a> <a href="https://codefund.app" data-target="powered_by_url" class="cf-powered-by" target="_blank" rel="noopener"> <em>ethical</em> ad by CodeFund <img data-src="impression_url"> </a> </span> </div>';
+      targetElement.querySelector('img[data-src="impression_url"]').src = 'https://codefund.app/display/103e18c0-6fc2-4059-ba16-697223a85d80.gif?template=horizontal&theme=unstyled';
+      targetElement.querySelectorAll('a[data-href="campaign_url"]').forEach(function (a) { a.href = 'https://codefund.app/impressions/103e18c0-6fc2-4059-ba16-697223a85d80/click?campaign_id=147'; });
+      targetElement.querySelector('a[data-target="powered_by_url"]').href = 'https://codefund.app/invite/4U7PKDC4gKo';
+      evt.detail = { status: 'ok', house: true };
+      detectUplift(1);
+    } catch (e) {
+      console.log('CodeFund detected an error! Please verify an element exists with id="codefund". ' + e.message);
+      evt.detail = { status: 'error', message: e.message };
+    }
+
+  }
+
+  render () {
+    const { props } = this
+
+    return (
   <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
     {/* <!-- Topbar  --> */}
 
@@ -15,7 +80,7 @@ const __Topbar = (props) => (
 
       <li className="nav-item">
         <span className="nav-link">
-          <div id="codefund" className="">CODEFUND AD</div>
+          <div id="codefund" ref={ref => this.codefundAdRef = ref}>CODEFUND AD</div>
         </span>
       </li>
 
@@ -155,6 +220,8 @@ const __Topbar = (props) => (
     {/* <!-- End of Topbar  --> */}
   </nav>
 )
+  }
+}
 
 const Topbar = connect(
   ({ user, isLoading, error, connection, instagram }) => ({ user, isLoading, error, connection, instagram }),
