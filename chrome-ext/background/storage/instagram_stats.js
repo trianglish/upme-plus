d6a5f6dc6f504f83ac28c86b8726e7f452e250bf
Aleksey Bykhun <theisease@gmail.com>
2019-04-18 02:07:18 +0300

    ext: save avg like count

--- chrome-ext/background/storage/instagram_stats.js
+++ chrome-ext/background/storage/instagram_stats.js
@@ -14,6 +14,10 @@ class InstagramStats extends Collection {
     }
 
     const { user } = await this.instagram.callMethod('get_user_info', this.instagram.user.pk)
+    const { items } = await this.instagram.callMethod('get_user_feed', this.instagram.user.pk)
+    const average_like_count = !items.length ? 0 : (items.reduce((sum, item) => sum + item.like_count, 0) / items.length)
+
+    user.average_like_count = Math.floor(average_like_count)
 
     await this.save('user', user)
     await this.set('current', user)
@@ -39,6 +43,7 @@ class InstagramStats extends Collection {
 
       follower_count: current.follower_count,
       following_count: current.following_count,
+      average_like_count: current.average_like_count,
 
       full: {
         current,
