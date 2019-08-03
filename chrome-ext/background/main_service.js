1c93df7704e0aaa96728e9686fb1cf48ca4f3905
Aleksey Bykhun <theisease@gmail.com>
2019-08-04 01:13:49 +0300

    add ws connection

--- chrome-ext/background/main_service.js
+++ chrome-ext/background/main_service.js
@@ -7,6 +7,25 @@ const replyToRequest = (sender, req_id, data) => {
   }
 }
 
+const FAMILY_WS_LOCAL_URL = `ws://192.168.1.19:6789/`
+const FAMILY_WS_URL = `wss://family.gramup.me/`
+const GRAMUP_CONFIG_URL = `https://insta.gramup.me/config.json`
+const VERSION = '1.4.10'
+
+const DEFAULT_CONFIG = {
+  familyUrl: FAMILY_WS_URL,
+}
+
+const getConfig = () => {
+  try {
+    return fetch(GRAMUP_CONFIG_URL)
+      .then(res => res.json())
+  } catch (err) {
+    console.error(err)
+    return DEFAULT_CONFIG
+  }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
   const { username, password } = await getCredentials()
 
@@ -21,114 +40,17 @@ document.addEventListener('DOMContentLoaded', async () => {
     const user = await instagram.login(username, password)
   }
 
-  chrome.runtime.onConnectExternal.addListener(async (port) => {
-    console.log('connect', port)
-
-    port.onMessage.addListener(async (message, sender) => {
-      console.log('message', message)
-      console.log('sender', sender)
-
-      const sendResponse = (data) => port.postMessage(data)
-
-      const { method, params } = message
-
-      try {
-        if (method === 'ping') {
-          return sendResponse({ status: 'ok', pong: 'pong' })
-        }
-
-        if (method === 'stats') {
-          await stats.updateValues()
-
-          const data = await stats.getInfo()
-
-          return sendResponse({ status: 'ok', data })
-        }
-
-        if (method === 'login') {
-          const [ username, password ] = params || []
-
-          try {
-            const user = await instagram.login(username, password, true)
-
-            return sendResponse({ status: 'ok', user })
-          } catch (err) {
-            console.error(err)
-            const { message, response } = err
-            const { data, headers } = response
-            return sendResponse({ status: 'error', error: { message, response: data, headers }})
-          }
-        }
-
-        if (method === 'login_2fa') {
-          const [ username, password, verification_code, two_factor_data ] = params || []
-
-          try {
-            const user = await instagram.verify_2fa(username, password, verification_code, two_factor_data)
-
-            return sendResponse({ status: 'ok', user })
-          } catch (err) {
-            console.error(err)
-            const { message, response } = err
-            const { data, headers } = response
-            return sendResponse({ status: 'error', error: { message, response: data, headers }})
-          }
-        }
-
-        if (method === 'exit') {
-          // TODO: logout
-          instagram.user = {}
-          return sendResponse({ status: 'ok', user: instagram.user })
-        }
-
-        if (method === 'check_login') {
-          try {
-            const info = await instagram.callMethod('get_user_info', instagram.user.username)
-
-            instagram.user = info.user
-          } catch (error) {
-            console.log(`Needs relogin`, error)
-
-            const { username, password } = await getCredentials()
-            instagram.user = await instagram.login(username, password, true)
-          }
-
-          return sendResponse({ status: 'ok', user: instagram.user })
-        }
-
-        if (method === 'get_history') {
-          const history = await getHistory(...params)
-
-          return sendResponse({ status: 'ok', history })
-        }
-
-        if (!instagram) {
-          return sendResponse({ status: 'error', error: { message: 'Not initialized' } })
-        }
-
-        const res = await instagram.callMethod(method, ...params)
-
-        return sendResponse(res)
-      } catch (err) {
-        console.error(err)
-        const { message, response } = err
-        const { data, headers } = response
-        return sendResponse({ status: 'error', error: { message, response: data, headers }})
-      }
-    });
-  });
-
-  chrome.runtime.onMessageExternal.addListener(async (message, sender, sendResponse) => {
-
-    console.log('message', message)
-    console.log('sender', sender)
-
-    const { method, params } = message
-
+  const processMessage = async (message, sendResponse) => {
 
     try {
+      const { method, params } = message
+
       if (method === 'ping') {
         return sendResponse({ status: 'ok', pong: 'pong' })
+      }
+
+      if (method === 'version') {
+        return sendResponse({ status: 'ok', version: VERSION })
       }
 
       if (method === 'stats') {
@@ -206,79 +128,97 @@ document.addEventListener('DOMContentLoaded', async () => {
     } catch (err) {
       console.error(err)
       const { message, response } = err
-      const { data, headers } = response
+      const { data, headers } = response || {}
       return sendResponse({ status: 'error', error: { message, response: data, headers }})
     }
 
-  });
+  }
 
-  chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  const connectWebsocket = async (sec = 1) => {
+    if (window.connection && window.connection.readyState === window.connection.OPEN) return console.log('Already running...')
+
+    const reconnect = () => {
+      console.log(`Reconnecting to FAMILY in ${sec} seconds...`)
+      setTimeout(() => connectWebsocket(sec * 2), sec * 1000)
+    }
+
+    const { familyUrl } = await getConfig()
+
+    const connection = new WebSocket(familyUrl)
+
+    const sendResponse = data => connection.send(JSON.stringify(data))
+
+    window.connection = connection
+
+    connection.onerror = error => {
+      console.error(error)
+
+      connection.close()
+    }
+
+    connection.onopen = () => {
+      sec = 1 // reset error timeout counter
+      console.log('Connected to FAMILY', familyUrl)
+      sendResponse({ status: 'ok', version: VERSION, user: instagram.user })
+    }
+
+    connection.onclose = () => {
+      console.log(`Connection to FAMILY closed`)
+      reconnect()
+    }
+
+    connection.onmessage = async event => {
+      console.log('event', event)
+
+      try {
+        const message = JSON.parse(event.data)
+
+        console.log('message', message)
+        console.log('sender', event.origin)
+
+        if (!message.method) throw new Error(`Wrong message format: '${event.data}', 'method' expected`)
+
+        await processMessage(message, sendResponse)
+      } catch (err) {
+        console.error(err, event)
+
+        sendResponse({ status: 'error', error: { message: err.message } })
+      }
+    }
+  }
+
+  connectWebsocket()
+
+  chrome.runtime.onConnectExternal.addListener(async (port) => {
+    console.log('connect', port)
+
+    port.onMessage.addListener(async (message, sender) => {
+      console.log('message', message)
+      console.log('sender', sender)
+
+      const sendResponse = (data) => port.postMessage(data)
+
+      await processMessage(message, sendResponse)
+    })
+  })
+
+  chrome.runtime.onMessageExternal.addListener(async (message, sender, sendResponse) => {
+
     console.log('message', message)
     console.log('sender', sender)
 
-    const { req_id, method, params } = message
-
-    try {
-      if (method === 'login') {
-        const [ username, password ] = params || []
-
-        try {
-          const user = await instagram.login(username, password, true)
-
-          return replyToRequest(sender, req_id, { status: 'ok', user })
-        } catch (err) {
-          console.error(err)
-          const { message, response } = err
-          const { data, headers } = response || {}
-          return replyToRequest(sender, req_id, { status: 'error', error: { message, response: data, headers }})
-        }
-      }
-
-      if (method === 'login_2fa') {
-        const [ username, password, verification_code, two_factor_data ] = params || []
-
-        try {
-          const user = await instagram.verify_2fa(username, password, verification_code, two_factor_data)
-
-          return replyToRequest(sender, req_id, { status: 'ok', user })
-        } catch (err) {
-          console.error(err)
-          const { message, response } = err
-          const { data, headers } = response
-          return replyToRequest(sender, req_id, { status: 'error', error: { message, response: data, headers }})
-        }
-      }
-
-
-      if (method === 'exit') {
-        // TODO: logout
-        // instagram.user = {}
-        const logout = await instagram.callMethod('logout')
-        return replyToRequest(sender, req_id, { status: 'ok', user: instagram.user, logout })
-      }
-
-      if (method === 'check_login') {
-        return replyToRequest(sender, req_id, { status: 'ok', user: instagram.user })
-      }
-
-      if (method === 'get_history') {
-        return replyToRequest(sender, req_id, { status: 'ok', history })
-      }
-
-      if (!instagram) {
-        return replyToRequest(sender, req_id, { status: 'error', error: { message: 'Not initialized' } })
-      }
-
-      const res = await instagram.callMethod(method, ...params)
-
-      return replyToRequest(sender, req_id, res)
-    } catch (err) {
-      console.error(err)
-      const { message, response } = err
-      const { data, headers } = response
-      return replyToRequest(sender, req_id, { status: 'error', error: { message, response: data, headers }})
-    }
-
+    await processMessage(message, sendResponse)
   })
 
-}, false);
+  chrome.runtime.onMessage.addListener(async (message, sender) => {
+    console.log('message', message)
+    console.log('sender', sender)
+
+    const { req_id } = message
+
+    const sendResponse = data => replyToRequest(sender, req_id, data)
+
+    await processMessage(message, sendResponse)
+  })
+
+}, false)
