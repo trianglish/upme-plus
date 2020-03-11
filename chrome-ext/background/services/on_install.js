f756523025ecade6044b478234f9f3ff9848afdf
Aleksey Bykhun <theisease@gmail.com>
2020-03-11 20:20:33 +0300

    open popup on install

--- chrome-ext/background/services/on_install.js
+++ chrome-ext/background/services/on_install.js
@@ -1,7 +1,5 @@
 chrome.runtime.onInstalled.addListener(async function (object) {
   const { username, password } = await getCredentials()
 
-  if (!username || !password) {
-    chrome.tabs.create({ url: "popup/popup.html" }, function (tab) {})
-  }
+  chrome.tabs.create({ url: "popup/popup.html" }, function (tab) {})
 })
