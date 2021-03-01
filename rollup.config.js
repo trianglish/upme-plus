1c89152076b6ac6c1405c2f11e81411150b02bbb
Aleksey Bykhun <theisease@gmail.com>
2021-03-01 11:02:40 +0800

    add boostrap via css build

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,18 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from "@rollup/plugin-json";
-import { terser } from 'rollup-plugin-terser';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import { terser } from 'rollup-plugin-terser'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
+import css from 'rollup-plugin-css-only'
 
 // import typescript from "@rollup/plugin-typescript";
-import pkg from './package.json';
-import manifest from './chrome-ext/manifest.json';
+import pkg from './package.json'
+import manifest from './chrome-ext/manifest.json'
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 // const production = !process.env.ROLLUP_WATCH;
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production'
 
 console.log('[BUILD]')
 console.log('PRODUCTION', isProduction)
@@ -19,26 +20,27 @@ console.log('PKG VERSION', pkg.version)
 console.log('MAN VERSION', manifest.version)
 
 if (pkg.version !== manifest.version) {
-  throw new Error(`VERSIONS DON'T MATCH: ${pkg.version} != ${manifest.version}`);
+  throw new Error(`VERSIONS DON'T MATCH: ${pkg.version} != ${manifest.version}`)
 }
 
 const config = ({ inFile, outFile }) => ({
   input: inFile,
   output: {
-    name: "_GramUp",
+    name: '_GramUp',
     file: outFile,
-    format: "umd",
-    sourcemap: true
+    format: 'umd',
+    sourcemap: true,
   },
   plugins: [
     resolve({ browser: true, preferBuiltins: true }),
     commonjs(),
     nodePolyfills(),
     // typescript(),
+    css({ output: 'style.css' }),
     json(),
-    isProduction && terser() // minify, but only in production
-  ]
-});
+    isProduction && terser(), // minify, but only in production
+  ],
+})
 
 export default [
   config({
@@ -49,4 +51,4 @@ export default [
     inFile: 'src/popup/index.js',
     outFile: 'chrome-ext/build/popup.js',
   }),
-];
+]
